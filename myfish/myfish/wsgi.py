34b240dd4ef1a6b1e80b4bf5b75ce3f1acd1a4d8
Home <Home>
2017-05-29 22:08:09 +0800

    updated

--- myfish/myfish/wsgi.py
+++ myfish/myfish/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+from os.path import join, dirname, abspath
 
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+sys.path.insert(0, PROJECT_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myfish.settings")
 
 application = get_wsgi_application()
