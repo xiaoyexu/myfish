08f3a299b760b0a2991f43b738d4b9b6465e4f51
Xu Xiaoye <xiaoye.xu@sap.com>
2017-06-02 16:50:04 +0800

    updated

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,1 +1,7 @@
-pwd
+#!/bin/bash
+sudo service uwsgi stop
+sudo service nginx stop
+sudo cp -R myfish/fish /home/xiaoye/demo/myfish/
+sudo cp -R myfish/static /home/xiaoye/demo/myfish/
+sudo service uwsgi start
+sudo service nginx start
