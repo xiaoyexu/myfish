75d9f9b4975bd7044a547fae4c8250323e4cb673
Xu Xiaoye <xiaoye.xu@sap.com>
2017-06-05 09:21:48 +0800

    updated

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,5 +3,7 @@ sudo service uwsgi stop
 sudo service nginx stop
 sudo cp -R myfish/fish /home/xiaoye/demo/myfish/
 sudo cp -R myfish/static /home/xiaoye/demo/myfish/
+sudo python /home/xiaoye/demo/myfish/manage.py makemigrations
+sudo python /home/xiaoye/demo/myfish/manage.py migrate
 sudo service uwsgi start
 sudo service nginx start
